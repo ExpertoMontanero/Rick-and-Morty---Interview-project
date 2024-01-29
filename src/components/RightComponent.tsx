07f9820b023ca1de178522b8c7d7c7a00f0c0717
Tomasz Zawadzki <tomek.zaw5@gmail.com>
2024-01-29 11:36:19 +0100

    data fetching from GraphQL bug fix
    pre-fetched data from Rick and Morty API

--- src/components/RightComponent.tsx
+++ src/components/RightComponent.tsx
@@ -1,7 +1,41 @@
 import "../styles/rightComponent.css";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function RightComponent() {
-  return <div className="background"></div>;
+import { useQuery, gql } from "@apollo/client";
+export default function RightComponent() {
+  const GET_EPISODES = gql`
+    query GetEpisodes {
+      episodes(filter: { episode: "S04" }) {
+        results {
+          episode
+          name
+          air_date
+        }
+      }
+    }
+  `;
+  function DisplayEpisodes() {
+    const { loading, error, data } = useQuery(GET_EPISODES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return data.episodes.results.map(({ episode, name, air_date }) => (
+      <div key={episode}>
+        <h3>{name}</h3>
+        <h3>{air_date}</h3>
+        <br />
+        <b>About this location:</b>
+        <p>{episode}</p>
+        <br />
+      </div>
+    ));
+  }
+  return (
+    <div>
+      <DisplayEpisodes />
+    </div>
+  );
 }
-
-export default RightComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+client
+  .query({
+    query: gql`
+      query GetEpisodes {
+        episodes(filter: { episode: "S04" }) {
+          results {
+            episode
+            name
+            air_date
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
